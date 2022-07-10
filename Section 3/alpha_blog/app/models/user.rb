6b0731cf5a270b99f63603a83f29ad41b3288b6b
pedroarouck <pedrohenrique.sales@hotmail.com>
2022-07-09 23:14:30 -0300

    inserindo validacoes de usuario

--- Section 3/alpha_blog/app/models/user.rb
+++ Section 3/alpha_blog/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    
+  validates :username, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { minimum:3, maximum: 25 }
+  VALID_EMAIL_REGEX = 
+  validates :email, presence: true, 
+                      uniqueness: { case_sensitive: false }, 
+                      length: { maximum: 125 },
+                      format: { with: VALID_EMAIL_REGEX }
 end
