35920ed41871b96af7a88eb96819293afef2da42
pedroarouck <pedrohenrique.sales@hotmail.com>
2022-07-16 10:10:14 -0300

    adicionando password

--- Section 3/alpha_blog/db/schema.rb
+++ Section 3/alpha_blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_024120) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_16_124251) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_10_024120) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
