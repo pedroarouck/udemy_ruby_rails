4f1a074236ec67b4133a25ba59a3c83f38cf8dd0
pedroarouck <pedrohenrique.sales@hotmail.com>
2022-07-16 18:05:06 -0300

    trabalhando com paginacao

--- Section 3/alpha_blog/app/controllers/users_controller.rb
+++ Section 3/alpha_blog/app/controllers/users_controller.rb
@@ -1,35 +1,44 @@
 class UsersController < ApplicationController
 
-    def new
-        @user = User.new
-    end
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 4)
+  end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  def index
+    @users = User.paginate(page: params[:page], per_page: 4)
+  end
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "Your account information was successfuly updated"
-        redirect_to articles_path
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfuly updated"
+      redirect_to @user
+    else
+      render 'edit',  status: :unprocessable_entity
+    end
+  end
+
+  def create 
+      @user = User.new(user_params)
+      if @user.save
+          flash[:notice] = "Welcome to the Alhpha Blog #{@user.username}, you have successfully signed up"
+          redirect_to articles_path
       else
-        render 'edit',  status: :unprocessable_entity
+          render 'new',  status: :unprocessable_entity
       end
-    end
+  end
 
-    def create 
-       @user = User.new(user_params)
-        if @user.save
-            flash[:notice] = "Welcome to the Alhpha Blog #{@user.username}, you have successfully signed up"
-            redirect_to articles_path
-        else
-            render 'new',  status: :unprocessable_entity
-        end
-    end
-
-    private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
