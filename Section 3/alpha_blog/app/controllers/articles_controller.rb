e5a91170d6884b80598013a889eda4485677f656
pedroarouck <pedrohenrique.sales@hotmail.com>
2022-07-16 21:23:17 -0300

    encerrando por hoje

--- Section 3/alpha_blog/app/controllers/articles_controller.rb
+++ Section 3/alpha_blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def show       
     end
@@ -17,7 +19,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save 
             flash[:notice] = "Article was created successfully."
             redirect_to @article
@@ -38,7 +40,7 @@ class ArticlesController < ApplicationController
 
     def destroy
         @article.destroy
-        redirect_to articles_path , status: :see_other
+        redirect_to articles_path, status: :see_other
     end
 
     private
@@ -51,4 +53,10 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title, :description)
     end
 
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
+    end
 end
